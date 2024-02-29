d5ee0ecab1437f4d4eea5f03ee50592dbdcebbf4
basit1215 <haizabdulbasit225@gmail.com>
2024-02-29 22:33:18 +0500

    updated

---
+++ app.js
@@ -0,0 +1,5 @@
+alert("Error! Please enter a valid password");
+alert("Welcome to JS Land...\nHappy Coding!");
+alert("Welcome to JS Land...");
+alert("Happy Coding!\n Prevent this page from creating additionals dialogs.");
+alert ("Hello... I can run JS through my web browser's console");
